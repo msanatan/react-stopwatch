7a3e8cbd190ff56a9df9c35a8b7b33ff860aa4cb
Marcus Sanatan <msanatan@gmail.com>
2017-08-05 19:25:25 -0400

    Add 3 fields for hours, minutes and seconds Also put their placeholder to 00. The user will enter the start times in the same component that counts down.

--- src/Timer.js
+++ src/Timer.js
@@ -4,14 +4,17 @@ import { Form, FormControl } from 'react-bootstrap';
 class Timer extends Component {
   render() {
     return (
-      <Form>
-        <FormControl>
+      <Form inline>
+        <FormControl
+          placeholder="00">
         </FormControl>
-
-        <FormControl>
+        <span>:</span>
+        <FormControl
+          placeholder="00">
         </FormControl>
-
-        <FormControl>
+        <span>:</span>
+        <FormControl
+          placeholder="00">
         </FormControl>
       </Form>
     );
