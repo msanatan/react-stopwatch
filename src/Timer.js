4f8f40a6838187099c96c3880a8fd8bf7b1dfd1f
Marcus Sanatan <msanatan@gmail.com>
2017-09-15 20:58:20 +0000

    Add time states to stopwatch Store the hours, minutes and seconds elapsed. Also add a leading 0 when they are less than 10.

--- src/Timer.js
+++ src/Timer.js
@@ -7,22 +7,35 @@ const Separator = () => {
 }
 
 class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  }
+
   render() {
     return (
       <div className='Timer'>
         <div className='Timer-display'>
           <Form inline className='Timer-display-form'>
-              <ControlLabel className='Timer-number'>00</ControlLabel>
+              <ControlLabel className='Timer-number'>{this.leadingZero(this.state.hours)}</ControlLabel>
               <Separator />
-              <ControlLabel className='Timer-number'>00</ControlLabel>
+              <ControlLabel className='Timer-number'>{this.leadingZero(this.state.minutes)}</ControlLabel>
               <Separator />
-              <ControlLabel className='Timer-number'>00</ControlLabel>
+              <ControlLabel className='Timer-number'>{this.leadingZero(this.state.seconds)}</ControlLabel>
           </Form>
         </div>
         <div className='Timer-buttons'>
         </div>
       </div>
     );
+  }
+
+  leadingZero(num) {
+    return num < 10 ? '0' + num : num;
   }
 }
 
