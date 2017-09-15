043e1a182f1024762b680e0f61ac791a2142b88b
Marcus Sanatan <msanatan@gmail.com>
2017-09-15 00:08:30 +0000

    Put wrapper around App component Will use a flexbox layout for this app, so using a wrapper to begin the flex layout. Also removed an unused dependency.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { Form, FormControl } from 'react-bootstrap';
 import Timer from './Timer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='.App'>
-        <Timer />
+      <div className='App-Container'>
+        <div className='App'>
+          <Timer />
+        </div>
       </div>
     );
   }
