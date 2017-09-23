779f4896d032a848fc105858db58a43f010ce355
Marcus Sanatan <msanatan@gmail.com>
2017-09-23 22:25:43 +0000

    Add switching buttons based on stopwatch state changes Determine if a button started and therefore show the stop and reset buttons. Time counted is currently determined by the difference between the first start time and the last time recorded. This is incorrect as the stopwatch continues to count the second since the start time even when it's stopped (because the difference of the start time and the current time increases proportionally). TODO: fix reset bug, may consider keeping state of only seconds.

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -10,23 +10,103 @@ class Stopwatch extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      startTime: null,
+      tick: null,
       hours: 0,
       minutes: 0,
       seconds: 0
     };
   }
-  
+
   getTimeSinceStart(startTime) {
     const now = Date.now();
-    const difference = now - startTime;
+    const difference = now - this.state.startTime;
     const seconds = Math.floor((difference / 1000) % 60);
     const minutes = Math.floor((difference / 1000 / 60) % 60);
     const hours = Math.floor((difference / 1000 / 60 / 60) % 24);
+    this.setState({
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    });
+  }
 
-    this.setState({ hours, minutes, seconds });
+  startCounter = () => {
+    clearInterval(this.state.tick);
+    this.setState({
+      startTime: Date.now(),
+      tick: setInterval(() => this.getTimeSinceStart(this.props.startTime), 1000)
+    });
+  }
+
+  stopCounter = () => {
+    clearInterval(this.state.tick);
+    this.setState({
+      tick: null
+    });
+  }
+
+  resetCounter = () => {
+    clearInterval(this.state.tick);
+    this.setState({
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      tick: null
+    });
+  }
+
+  resumeCounter = () => {
+    clearInterval(this.state.tick);
+    this.setState({
+      tick: setInterval(() => this.getTimeSinceStart(this.props.startTime), 1000)
+    });
   }
 
   render() {
+    let buttons = null;
+    let started = this.state.hours > 0 || this.state.minutes > 0 || this.state.seconds > 0;
+    if (!this.state.tick && !started) {
+      buttons = (
+          <ButtonGroup justified>
+            <ButtonGroup className='Stopwatch-button-group'>
+              <Button bsStyle='info' bsSize='large' block
+                className='Stopwatch-button'
+                onClick={this.startCounter}>Start</Button>
+            </ButtonGroup>
+          </ButtonGroup>
+        );
+    } else if (!this.state.tick && started) {
+      buttons = (
+          <ButtonGroup justified>
+            <ButtonGroup className='Stopwatch-button-group'>
+              <Button bsStyle='info' bsSize='large' block
+                className='Stopwatch-button'
+                onClick={this.resumeCounter}>Resume</Button>
+            </ButtonGroup>
+            <ButtonGroup className='Stopwatch-button-group'>
+                <Button bsSize='large' block
+                  className='Stopwatch-button'
+                  onClick={this.resetCounter}>Reset</Button>
+              </ButtonGroup>
+          </ButtonGroup>
+        );
+    } else {
+        buttons = (
+          <ButtonGroup justified>
+            <ButtonGroup className='Stopwatch-button-group'>
+              <Button bsStyle='danger' bsSize='large' block
+                className='Stopwatch-button'
+                onClick={this.stopCounter}>Stop</Button>
+            </ButtonGroup>
+            <ButtonGroup className='Stopwatch-button-group'>
+                <Button bsSize='large' block
+                  className='Stopwatch-button'
+                  onClick={this.resetCounter}>Reset</Button>
+              </ButtonGroup>
+          </ButtonGroup>
+        );
+    }
     return (
       <div className='Stopwatch'>
         <div className='Stopwatch-display'>
@@ -39,14 +119,7 @@ class Stopwatch extends Component {
           </Form>
         </div>
         <div className='Stopwatch-buttons'>
-          <ButtonGroup justified>
-            <ButtonGroup className='Stopwatch-button-group'>
-              <Button bsStyle='info' bsSize='large' block className='Stopwatch-button'>Start</Button>
-            </ButtonGroup>
-            <ButtonGroup className='Stopwatch-button-group'>
-              <Button bsSize='large' block className='Stopwatch-button'>Reset</Button>
-            </ButtonGroup>
-          </ButtonGroup>
+            {buttons}
         </div>
       </div>
     );
